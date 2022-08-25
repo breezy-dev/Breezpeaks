858043be81b43aaf367a04c402cb11c036c89aa7
Joan Ndong <jo-wilfried11@hotmail.com>
2022-08-25 02:31:25 +0100

    initalisation du projet; creation des fichiers server.js et db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-mongoose.connect(
-  "mongodb+srv://breezydev:<password>@cluster0.0bojka6.mongodb.net/test",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-).then (() => console.log('Connected to MongoDB'))
+mongoose
+    .connect(
+        "mongodb+srv://breezydev:<password>@cluster0.0bojka6.mongodb.net/test",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Failed to connect to MongoDB", err))
