fb3cc76e2cabc8916f95d16e0dc52782a0db12ff
Joan Ndong <jo-wilfried11@hotmail.com>
2022-08-25 02:25:27 +0100

    initalisation du projet; creation des fichiers server.js et db.js

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://breezydev:<password>@cluster0.0bojka6.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+).then (() => console.log('Connected to MongoDB'))
